14a382527993e9d8fd1e16b2caccdd4e0afd7af0
no-touch-fish <jh103@illinois.edu>
2024-07-08 18:08:17 +0800

    add

---
+++ math_data.py
@@ -0,0 +1,57 @@
+import json
+
+def process_qa_pair(qa_pair):
+
+    question = qa_pair['question']
+    answer = qa_pair['answer']
+
+    # find the question (last sentense) in question
+    index = question.rfind('?')
+    while (question[index] != '.'):
+        index -= 1
+        # the case like "If cake is 1 dollar, how much is the cake?" -> should sepeate with ","
+        if index == 0:
+            index = question.rfind('?')
+            while (question[index] != ','):
+                index -= 1
+            break
+    # add 1 to avoid the "."
+    last_question = question[index + 1:].strip()
+    context = question[:index+1].strip()
+
+    # get the question in answer
+    answer_parts = answer.split('\n')
+    question_list = []
+    answer_list = []
+    for part in answer_parts:
+        index = part.rfind('?')
+        question_list.append(part[:index + 1].strip())
+        answer_list.append(part[index + 1:].strip())
+    # print(answer_parts)
+    # print(question_list)
+    # print(answer_list)
+
+    # create new Q-A pair
+    additional_part = 'Answer the question one by one.'
+    new_qa_pair = {
+        'question': context + ' ' + ' '.join(question_list).strip() + ' ' + last_question + ' ' + additional_part,
+        'answer': ' '.join(answer_list).strip()
+    }
+    # print(new_qa_pair)
+    return new_qa_pair
+
+def process_json(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    processed_data = [process_qa_pair(json.loads(line)) for line in lines]
+    # print(processed_data)
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for qa_pair in processed_data:
+            f.write(json.dumps(qa_pair, ensure_ascii=False) + '\n')
+
+
+input_file = 'dataset/test.json'
+output_file = 'dataset/processed_test.json'
+
+process_json(input_file, output_file)
+
